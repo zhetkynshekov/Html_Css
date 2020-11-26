5a85fcdf6750029d30b2c84369dd1a368d418db7
zhetkynshekov <zhrk2407@gmail.com>
2020-11-26 16:42:04 +0500

    Html Css files

---
+++ Test04/index.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,500,900" rel="stylesheet"> 
+    <title>New peoject</title>
+  </head>
+  <body>
+    <header class="header-menu">
+      <img src="img/header-menu/logo_w.png" alt="">
+    </header>
+    <div class="container">
+      <nav class="nav">
+        <h2>
+          1.hyper-threading — гиперпоточность, қай компания технологиясы?
+        </h2>
+        <ul class="otveti">
+          <li><a class="b" href="javascript:void(0)">a.	Intel.</a></li>
+          <li><a class="a" href="javascript:void(0)">b.	AMD</a></li>
+          <li><a class="a" href="javascript:void(0)">c.	MediaTek</a></li>
+          <li><a class="a" href="javascript:void(0)">d.	Snapdragon</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	Exynos</a></li>
+        </ul>
+      </nav>
+      <nav class="nav">
+        <h2>2.	Десктопты видеокартаны ноутбукқа қосуға мүмкіндік беретін интерфейс.
+        </h2>
+        <ul class="otveti">
+          <li><a class="a" href="javascript:void(0)">a.	USB Type - C </a></li>
+          <li><a class="a" href="javascript:void(0)">b.	Thunderbolt 1</a></li>
+          <li><a class="b" href="javascript:void(0)">c.	Thunderbolt 3</a></li>
+          <li><a class="a" href="javascript:void(0)">d.	USB 3.0</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	Display port</a></li>
+        </ul>
+      </nav>
+      <nav class="nav">
+        <h2>
+          3.	L3 кэш қай процессорда жоқ?
+        </h2>
+        <ul class="otveti">
+          <li><a class="a" href="javascript:void(0)">a.	AMD FX-8300</a></li>
+          <li><a class="a" href="javascript:void(0)">b.	Intel core - i7</a></li>
+          <li><a class="a" href="javascript:void(0)">c.	Intel core – i3</a></li>
+          <li><a class="b" href="javascript:void(0)">d.	Intel core quad q6600.</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	AMD Ryzen-7</a></li>
+        </ul>
+      </nav>
+      <nav class="nav">
+        <h2>
+          4.	3D Xpoint технологиясы қай компания продукттерінде қолданылады?
+        </h2>
+        <ul class="otveti">
+          <li><a class="a" href="javascript:void(0)">a.	AMD</a></li>
+          <li><a class="a" href="javascript:void(0)">b.	HP</a></li>
+          <li><a class="a" href="javascript:void(0)">c.	Samsung</a></li>
+          <li><a class="b" href="javascript:void(0)">d.	Intel</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	Kingston</a></li>
+        </ul>
+      </nav>
+      <nav class="nav">
+        <h2>
+          5.	Thunderbolt 3 интерфейсінің өткізгіштік қабылеті кандай?
+        </h2>
+        <ul class="otveti">
+          <li><a class="a" href="javascript:void(0)">a.	20 гб/с</a></li>
+          <li><a class="a" href="javascript:void(0)">b.	35 гб/с</a></li>
+          <li><a class="a" href="javascript:void(0)">c.	60 гб/с</a></li>
+          <li><a class="b" href="javascript:void(0)">d.	40 гб/с</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	100 мб/с</a></li>
+        </ul>
+      </nav>
+      <nav class="nav">
+        <h2>
+          6.	SSD дисктерінде қолданылаьын жады типі қандай?
+        </h2>
+        <ul class="otveti">
+          <li><a class="a" href="javascript:void(0)">a.	GDDR</a></li>
+          <li><a class="a" href="javascript:void(0)">b.	DDR</a></li>
+          <li><a class="b" href="javascript:void(0)">c.	MLC</a></li>
+          <li><a class="a" href="javascript:void(0)">d.	L2</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	SRAM</a></li>
+        </ul>
+      </nav>
+      <nav class="nav">
+        <h2>
+          7.	Жиілігі 2133 МГц DDR4 жадысының максималды өткізгіштік қаблеті.
+        </h2>
+        <ul class="otveti">
+          <li><a class="a" href="javascript:void(0)">a.	25600 Мб/с</a></li>
+          <li><a class="b" href="javascript:void(0)">b.	17064 Мб/с</a></li>
+          <li><a class="a" href="javascript:void(0)">c.	12800 Мб/с</a></li>
+          <li><a class="a" href="javascript:void(0)">d.	51200 Мб/с</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	10664 Мб/с</a></li>
+        </ul>
+      </nav>
+      <nav class="nav">
+        <h2>
+          8.	Мобиьді процессорды тап.
+        </h2>
+        <ul class="otveti">
+          <li><a class="b" href="javascript:void(0)">a.	I5-7200U</a></li>
+          <li><a class="a" href="javascript:void(0)">b.	I7-2600K
+            </a></li>
+          <li><a class="a" href="javascript:void(0)">c.	I7-3770K</a></li>
+          <li><a class="a" href="javascript:void(0)">d.	I9-9900K</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	Xeon X3440</a></li>
+        </ul>
+      </nav>
+      <nav class="nav">
+        <h2>
+          9.	Ray Tracing технологиясы қолданылатын видео адаптер.
+        </h2>
+        <ul class="otveti">
+          <li><a class="a" href="javascript:void(0)">a.	GTX 1080Ti</a></li>
+          <li><a class="a" href="javascript:void(0)">b.	GTX 950M</a></li>
+          <li><a class="b" href="javascript:void(0)">c.	RTX 2080Ti</a></li>
+          <li><a class="a" href="javascript:void(0)">d.	GTX 1660</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	MX 150</a></li>
+        </ul>
+      </nav>
+      <nav class="nav">
+        <h2>
+          10.	8 ядролы серверлік процессор.
+        </h2>
+        <ul class="otveti">
+          <li><a class="a" href="javascript:void(0)">10.	8 ядролы серверлік процессор.</a></li>
+          <li><a class="a" href="javascript:void(0)">b.	Xeon E5460</a></li>
+          <li><a class="b" href="javascript:void(0)">c.	Xeon E5-2670</a></li>
+          <li><a class="a" href="javascript:void(0)">d.	I9-9900K</a></li>
+          <li><a class="a" href="javascript:void(0)">e.	Xeon x3440</a></li>
+        </ul>
+      </nav>
+    </div>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/main.js"></script>
+  </body>
---
+++ Test04/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.otveti li a.a').click(function(){
+        $(this).toggleClass('False');
+    });
+});
+$(function(){
+    $('.otveti li a.b').click(function(){
+        $(this).toggleClass('True');
+    });
+});
